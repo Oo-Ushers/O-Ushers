99c7920ca746c629a43fc54806f0268d0169174b
YousefMohamed19 <yousef01121977646@gmail.com>
2026-03-04 23:28:19 +0200

    feat: Add User model definition and core application enums for roles, status, language, and event categories.

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,38 +1,87 @@
 import { DataTypes } from 'sequelize';
 import { sequelize } from '../connection.js';
-import { status } from '../../src/utils/constant/enums.js';
+import { eventCategories, language, roles } from '../../src/utils/constant/enums.js';
 
 export const User = sequelize.define(
   'User',
   {
     id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
+    },
+    fullName: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     userName: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
     },
+    role: {
+      type: DataTypes.ENUM(...Object.values(roles)),
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-      set(value) {
-        this.setDataValue('email', value.toLowerCase().trim());
-      },
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    status: {
+    mobileNumber: {
       type: DataTypes.STRING,
-      defaultValue: status.PENDING,
-      validate: {
-        isIn: [Object.values(status)],
+      allowNull: false,
+      unique: true,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    organizationId: {
+      type: DataTypes.ARRAY(DataTypes.UUID),
+      allowNull: false,
+      references: {
+        model: 'organizations',
+        key: 'id',
       },
+    },
+    experience: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    languages: {
+      type: DataTypes.ARRAY(DataTypes.ENUM(...Object.values(language))),
+      allowNull: false,
+    },
+    eventCategories: {
+      type: DataTypes.ARRAY(DataTypes.ENUM(...Object.values(eventCategories))),
+      allowNull: false,
+    },
+    portfolio: {
+      type: DataTypes.ARRAY(DataTypes.JSON),
+      allowNull: false,
+    },
+    // portfolio picture avatar default picture
+    portfolioPicture: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    isEmailVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    // admin verification
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    rate: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
     },
   },
   {
